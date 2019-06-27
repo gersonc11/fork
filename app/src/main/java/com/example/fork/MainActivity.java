ddb92f3e7417bac7a09672f25728677715b697e1
gersonc11 <gcruz1776@gmail.com>
2019-06-27 14:45:38 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fork/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.fork;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+
+}
